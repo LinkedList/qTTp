2c161e3108c25ed22cf0390527367831626c858e
Martin Macko <macko.mar@gmail.com>
2018-01-27 17:42:03 +0100

    Code cleanup

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys
-import requests
 import json
+
+import requests
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow)
 from json2html import *
 
 from ui import Ui_MainWindow
 
-from PyQt5.QtWidgets import (
-    QWidget, QLineEdit, QApplication, QPushButton, QTextBrowser, QDialog, QMainWindow)
 
 class Qttp(Ui_MainWindow):
-    def __init__(self, window):
+    def __init__(self, w):
         Ui_MainWindow.__init__(self)
-        self.setupUi(window)
+        self.setupUi(w)
 
         self.sendButton.clicked.connect(self.request)
         self.url.returnPressed.connect(self.request)
@@ -23,9 +23,7 @@ class Qttp(Ui_MainWindow):
         r = requests.get('https://' + self.url.text())
         j = r.text
         parse = json.loads(j)
-        self.responseText.setHtml(json2html.convert(json = parse))
-
-
+        self.responseText.setHtml(json2html.convert(json=parse))
 
 
 if __name__ == '__main__':
@@ -34,39 +32,3 @@ if __name__ == '__main__':
     prog = Qttp(window)
     window.show()
     sys.exit(app.exec_())
-
-# class Example(QWidget):
-
-#     def __init__(self):
-#         super().__init__()
-
-#         self.initUI()
-
-#         def initUI(self):
-
-#             self.qle = QLineEdit(self)
-#             button = QPushButton("Send", self)
-#             button.move(0, 50)
-#             button.clicked.connect(self.buttonClicked)
-
-#             self.browser = QTextBrowser(self)
-#             self.browser.move(0, 100)
-
-#             self.qle.move(0, 0)
-
-#             self.setGeometry(300, 300, 280, 170)
-#             self.setWindowTitle('QLineEdit')
-#             self.show()
-
-#         def buttonClicked(self):
-#             print(self.qle.text())
-#             r = requests.get('https://' + self.qle.text())
-#             print(r.text)
-#             self.browser.setPlainText(r.text)
-
-
-# if __name__ == '__main__':
-
-#     app = QApplication(sys.argv)
-#     ex = Example()
-#     sys.exit(app.exec_())
