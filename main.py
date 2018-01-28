724e46b7c40830248b64f76aefcf0259e7768096
Martin Macko <macko.mar@gmail.com>
2018-01-28 15:09:12 +0100

    We have history now, and it's clickable

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ import sys
 import json
 
 import requests
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import (
-    QApplication, QMainWindow)
+    QApplication, QMainWindow, QListWidgetItem)
 
 from ui import Ui_MainWindow
 
+class Req(object):
+        def __init__(self, method, protocol, url):
+            super(Req, self).__init__()               
+            self.method = method
+            self.url = url
 
 class Qttp(Ui_MainWindow):
     def __init__(self, w):
@@ -19,9 +25,17 @@ class Qttp(Ui_MainWindow):
 
         self.sendButton.clicked.connect(self.request)
         self.url.returnPressed.connect(self.request)
+        self.historyList.doubleClicked.connect(self.setFromHistory)
 
     def request(self):
-        r = requests.request(method=self.method.currentText(), url='https://' + self.url.text())
+        method = self.method.currentText()
+        url = self.url.text()
+        protocol = 'https'
+        r = requests.request(method=method, url=protocol+"://"+url)
+        historyItem = QListWidgetItem()
+        historyItem.setText(method + " " + url)
+        historyItem.setData(QtCore.Qt.UserRole, Req(method, protocol, url))
+        self.historyList.insertItem(0, historyItem)
         headers = r.headers
         headersText = ""
         for key in headers:
@@ -31,6 +45,12 @@ class Qttp(Ui_MainWindow):
         dump = json.dumps(obj = parse, indent=4).replace(" ", "&nbsp;").replace("\n", "<br />")
         self.responseText.setHtml(dump)
         self.headersText.setHtml(headersText)
+
+    def setFromHistory(self, item):
+        req = item.data(QtCore.Qt.UserRole)
+        self.url.setText(req.url)
+        index = self.method.findText(req.method)
+        self.method.setCurrentIndex(index)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
