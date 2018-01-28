7021cf187bb404150975ce2f9f921232e98b7531
Martin Macko <macko.mar@gmail.com>
2018-01-28 21:32:01 +0100

    And we have basis for collections

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ import json
 
 import requests
 from PyQt5 import QtCore
+from PyQt5.QtGui import (QStandardItemModel, QStandardItem)
 from PyQt5.QtWidgets import (
-    QApplication, QMainWindow, QListWidgetItem)
+        QApplication, QMainWindow, QListWidgetItem, QMenu)
 
 from ui import Ui_MainWindow
 
 class Req(object):
-        def __init__(self, method, protocol, url):
-            super(Req, self).__init__()               
-            self.method = method
-            self.url = url
+    def __init__(self, method, protocol, url):
+        super(Req, self).__init__()               
+        self.method = method
+        self.url = url
 
 class Qttp(Ui_MainWindow):
     def __init__(self, w):
@@ -26,7 +27,24 @@ class Qttp(Ui_MainWindow):
         self.sendButton.clicked.connect(self.request)
         self.url.returnPressed.connect(self.request)
         self.historyList.doubleClicked.connect(self.setFromHistory)
+        self.historyList.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.historyList.customContextMenuRequested.connect(self.historyMenu)
         self.splitter.setSizes([1, 3])
+
+        self.collectionsModel = QStandardItemModel()
+        self.collectionsModel.appendRow(QStandardItem("Default"))
+        self.collectionsTree.setModel(self.collectionsModel)
+        self.collectionsTree.header().hide()
+
+    def addCollectionItem(self, collection, item):
+        items = self.collectionsModel.findItems(collection)
+        parent = None
+        if not items:
+            parent = QStandardItem(collection)
+        else:
+            parent = items.pop(0)
+
+        parent.appendRow(QStandardItem(item.method + " " + item.url))
 
     def request(self):
         method = self.method.currentText()
@@ -52,6 +70,14 @@ class Qttp(Ui_MainWindow):
         self.url.setText(req.url)
         index = self.method.findText(req.method)
         self.method.setCurrentIndex(index)
+
+    def historyMenu(self, position):
+        menu = QMenu()
+        saveAction = menu.addAction("Save")
+        action = menu.exec_(self.historyList.mapToGlobal(position))
+        if action == saveAction:
+            item = self.historyList.itemAt(position)
+            self.addCollectionItem("Default", item.data(QtCore.Qt.UserRole))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
