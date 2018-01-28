476d4363f32afa46835c32813fb1357fc758941b
Martin Macko <macko.mar@gmail.com>
2018-01-28 12:20:13 +0100

    There are headers now + ui changes

--- main.py
+++ main.py
@@ -15,17 +15,22 @@ class Qttp(Ui_MainWindow):
     def __init__(self, w):
         Ui_MainWindow.__init__(self)
         self.setupUi(w)
+        self.url.setFocus()
 
         self.sendButton.clicked.connect(self.request)
         self.url.returnPressed.connect(self.request)
 
     def request(self):
         r = requests.get('https://' + self.url.text())
+        headers = r.headers
+        headersText = ""
+        for key in headers:
+            headersText += "<b>" + key +"</b>" + ": " + headers[key] + "<br />"
         j = r.text
         parse = json.loads(j)
         dump = json.dumps(obj = parse, indent=4).replace(" ", "&nbsp;").replace("\n", "<br />")
-        print(dump)
         self.responseText.setHtml(dump)
+        self.headersText.setHtml(headersText)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
