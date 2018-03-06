99042b27d2f12fec6c91b0ee92f989bd5a5f87b9
Martin Macko <macko.mar@gmail.com>
2018-03-06 20:22:37 +0100

    We now have checkbox in the key value editor

--- key_value_editor.py
+++ key_value_editor.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QWidget, QTableWidgetItem
+from PyQt5.QtCore import pyqtSignal, Qt
 from key_value_editor_ui import Ui_keyValueEditor
 
 
@@ -8,3 +8,11 @@ class KeyValueEditor(QWidget, Ui_keyValueEditor):
     def __init__(self):
         super(KeyValueEditor, self).__init__()
         self.setupUi(self)
+
+        self.table = self.keyValueEditorTable
+
+        checkbox = QTableWidgetItem(True)
+        checkbox.setCheckState(Qt.Checked)
+        self.table.setItem(0, 0, checkbox)
+        self.table.setColumnWidth(0, 28)
+
