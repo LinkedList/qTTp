b16ebb4d9e9a0215dea833c3fec3167921a7b45e
Martin Macko <macko.mar@gmail.com>
2018-03-07 21:20:06 +0100

    We now have dynamically added rows in key value editor

--- key_value_editor.py
+++ key_value_editor.py
@@ -10,9 +10,27 @@ class KeyValueEditor(QWidget, Ui_keyValueEditor):
         self.setupUi(self)
 
         self.table = self.keyValueEditorTable
+        self.table.setColumnWidth(0, 28)
+        self.table.setItem(0, 0, self.createCheckBoxItem())
+        self.table.cellChanged.connect(self.maybeAddRow)
 
+    def createCheckBoxItem(self):
         checkbox = QTableWidgetItem(True)
         checkbox.setCheckState(Qt.Checked)
-        self.table.setItem(0, 0, checkbox)
-        self.table.setColumnWidth(0, 28)
+        return checkbox
+
+    def maybeAddRow(self):
+        count = self.table.rowCount()
+        if count == self.countRowsWithKeys():
+            self.table.insertRow(count)
+            self.table.setItem(count, 0, self.createCheckBoxItem())
+
+    def countRowsWithKeys(self):
+        rowCount = 0
+        for row in range(0, self.table.rowCount()):
+            item = self.table.item(row, 1)
+            if item != None and item.text() != "":
+                rowCount += 1
+
+        return rowCount
 
