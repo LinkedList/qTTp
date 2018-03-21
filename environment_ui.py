ffcf7eebe58ecadc3dcecc6c83dc4d10755abf84
Martin Macko <macko.mar@gmail.com>
2018-03-21 22:21:01 +0100

    Environment UI python file

---
+++ environment_ui.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'enironment.ui'
+#
+# Created by: PyQt5 UI code generator 5.7
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_environmentForm(object):
+    def setupUi(self, environmentForm):
+        environmentForm.setObjectName("environmentForm")
+        environmentForm.resize(400, 300)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(environmentForm)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.environmentCombo = QtWidgets.QComboBox(environmentForm)
+        self.environmentCombo.setObjectName("environmentCombo")
+        self.environmentCombo.addItem("")
+        self.horizontalLayout.addWidget(self.environmentCombo)
+        self.environmentEdit = QtWidgets.QPushButton(environmentForm)
+        self.environmentEdit.setObjectName("environmentEdit")
+        self.horizontalLayout.addWidget(self.environmentEdit)
+
+        self.retranslateUi(environmentForm)
+        QtCore.QMetaObject.connectSlotsByName(environmentForm)
+
+    def retranslateUi(self, environmentForm):
+        _translate = QtCore.QCoreApplication.translate
+        environmentForm.setWindowTitle(_translate("environmentForm", "Form"))
+        self.environmentCombo.setItemText(0, _translate("environmentForm", "New Environment"))
+        self.environmentEdit.setText(_translate("environmentForm", "PushButton"))
+
