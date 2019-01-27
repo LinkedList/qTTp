ab8d3142a8607a3876faf716aeb3a9b7c5cdf7a6
Martin Macko <macko.mar@gmail.com>
2019-01-27 12:46:10 +0100

    WIP

--- environment.py
+++ environment.py
@@ -1,8 +1,18 @@
 from environment_ui import Ui_environmentForm
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QDialog
+from key_value_editor import KeyValueEditor
 
 
 class EnvironmentSwitcher(QWidget, Ui_environmentForm):
     def __init__(self):
         super(EnvironmentSwitcher, self).__init__()
         self.setupUi(self)
+        
+        self.environmentEdit.clicked.connect(self.editCurrEnvironment)
+
+    def editCurrEnvironment(self):
+        self.dialog = QDialog()
+        self.dialog.exec_()
+        kv = KeyValueEditor()
+        self.dialog.addWidget(kv)
+        self.dialog.show()
